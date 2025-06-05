09497090f7d4c988b9c48cd7a345dee3df158773
link-ino <t-ino@link-si.co.jp>
2025-06-05 17:22:52 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP_KADAI_007</title>
+</head>
+<body>
+
+<?php
+
+       // 連想配列に値を代入する
+       $test_data = ['name' => 'orion', 'price' => 200, 'weight' => 160];
+
+       // 連想配列の値を出力する
+       print_r($test_data);
+
+?>
+
+</body>
+</html>
+
